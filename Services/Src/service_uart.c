b33a864b2e7d4e944c5f93d11d278e650b2dec70
matsoz <55518409+matsoz@users.noreply.github.com>
2022-05-04 21:32:53 -0300

    Update service_uart.c

--- Services/Src/service_uart.c
+++ Services/Src/service_uart.c
@@ -31,19 +31,19 @@ int USART2_addToQueue(char *msg){
 	//Add to queue
 	USART2_msg2Queue = (USART_message_t*) osPoolAlloc(mpool);
 
-	strcpy(USART2_msg2Queue->message, msg); // Random str
+	strcpy(USART2_msg2Queue->message, msg);
 	osMessagePut(queueUSART2Handle, (uint32_t) USART2_msg2Queue, 0);
 
 	return 0;
 }
 
 int USART2_printFromQueue(){
-
 	//Check if USART is not busy before attempt to consume from queue
 	if(huart2.gState != HAL_UART_STATE_BUSY_TX && huart2.gState != HAL_UART_STATE_BUSY_TX_RX ){
-		//Consume from queue
+		//Check queue and consume if available
 		osEvent evt = osMessageGet(queueUSART2Handle, osWaitForever);
 
+		//If there's data to consume, proceed
 		if (evt.status == osEventMessage)
 		{
 			USART_message_t *msgFromQueue = (USART_message_t*) evt.value.p;
@@ -52,7 +52,7 @@ int USART2_printFromQueue(){
 			// Transmit prepared message to USART2
 			#if(UART_USE_IT == 0)
 				//Do not use UART interruption to send data (blocking)
-				HAL_UART_Transmit( &huart2,	(uint8_t*) msgFromQueue, strlen(msgFromQueue), 5000);
+				HAL_UART_Transmit( &huart2,	(uint8_t*) msgFromQueue, strlen((char*)msgFromQueue), 5000);
 			#else
 				//Use UART interruption to send data (not blocking)
 				HAL_UART_Transmit_IT(&huart2,(uint8_t*) msgFromQueue, strlen((char*)msgFromQueue));
