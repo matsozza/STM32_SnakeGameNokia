c192e39ab519a619fec2b690580139a518bf3162
matsoz <55518409+matsoz@users.noreply.github.com>
2022-05-08 22:09:44 -0300

    LCD Level 1 services - ASCII Char

--- Services/Inc/service_lcd.h
+++ Services/Inc/service_lcd.h
@@ -8,28 +8,32 @@
  *      Author: Matheus Sozza
  */
 
-#ifndef INC_CORE_HL_SERVICES_UART_HL_SERVICES_H_
-#define INC_CORE_HL_SERVICES_UART_HL_SERVICES_H_
+#ifndef INC_CORE_SERVICES_LCD_SERVICES_H_
+#define INC_CORE_SERVICES_LCD_SERVICES_H_
 
 /* Includes ------------------------------------------------------------------*/
 #include "cmsis_os.h"
-
+#include "service_lcd_fonts.h"
 
 /* Defines -------------------------------------------------------------------*/
 #define SPI_USE_IT 1
-
+#define LCD_sendCommand(cmd)	 LCD_sendByteSPI(cmd,GPIO_PIN_RESET);
+#define LCD_sendData(cmd)		 LCD_sendByteSPI(cmd,GPIO_PIN_SET);
 
 /* Typedef declare -----------------------------------------------------------*/
 typedef struct{
 	char message[16];
 } USART_message_t;
 
-
 /* Function prototypes -------------------------------------------------------*/
-extern int LCD_sendData(uint8_t data);
-extern int LCD_sendCommand(uint8_t command);
+//Level 0 - SPI communication & startup
+extern int LCD_sendByteSPI(uint8_t data, uint8_t DC);
 extern int LCD_initializeConfigs();
-#endif /* INC_CORE_HL_SERVICES_UART_HL_SERVICES_H_ */
+
+//Level 1 - Graph manipulation & ASCII Chars
+extern int LCD_writeASCIIChar(char ASCII_char);
+
+#endif /* INC_CORE_SERVICES_LCD_SERVICES_H_ */
 
 
 
